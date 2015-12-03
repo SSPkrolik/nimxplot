526cf5a8fc291032df768c2f9b0c669c13ea6c58
Rostyslav Dzinko <rostislav.dzinko@gmail.com>
2015-12-03 20:44:13 +0200

    Added y=f(x) data mdoel with color support

--- nimxplot/dataxy.nim
+++ nimxplot/dataxy.nim
@@ -1,3 +1,8 @@
-##
+## Data model for plotting
+import nimx.types
 
-type ModelXY*[T] = seq[tuple[x: T, y: T]]  ## y=f(x) discrete data model
+type ModelXY*[T] = seq[tuple[x: T, y: T]]
+  ## y=f(x) discrete data model
+
+type ModelXYColor*[T] = seq[tuple[x: T, y: T, color: Color]]
+  ## t=f(x) discrete data model with colored dots
